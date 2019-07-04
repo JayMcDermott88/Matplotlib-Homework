37a640ee62f6c29db824fdea7ab4183bc96e16d4
JayMcDermott88 <jay.mcdermott93@gmail.com>
2019-07-04 14:35:28 -0500

    update

--- Conclusions.py
+++
@@ -1,8 +0,0 @@
-#Difficult to draw conclusions w/out scatter plot. But using the provided sample graph as a template....
-
-#1) There is a slight positive correlation between the drivers and fare. More drivers in a city, the lower the average fare as /
-    #the competitve for rides becomes more severe 
-
-#2) Cost is also likely associated with distance and as fare in rural environment is likely associated with distance per ride resulting in higher average fare
-
-#3) Utilization in urban environments directly correspnds with drivers and total rides
